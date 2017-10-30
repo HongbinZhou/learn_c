f8289cc87b641c4dfe517ca47c9ac02432405d88
Hongbin Zhou <hongbinzhou@hotmail.com>
2017-10-30 11:08:56 +0800

    add python encode example.

--- chinese.c
+++ chinese.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 
+/* check unicode in python:
+   s = "今天".encode("utf-8")
+   s = [oct(i) for i in s]
+ */
 int main() {
   unsigned char str[10] = "今天";      /* in oct: \344\273\212\345\244\251  */
   const unsigned char ref[10] = {0344, 0273, 0212, 0345, 0244, 0251}; /* leading 0 for octal numbers in C */
